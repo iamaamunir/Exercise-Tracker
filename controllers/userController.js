572424e6ec8656428e78de6560ae4b5e5b4c9261
iamaamunir <abdullahimunir3@gmail.com>
2022-12-23 15:34:47 +0100

    feat:GET users

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,19 @@ async function createUser(req, res, next) {
   try {
     const username = req.body.username;
     const user = await userModel.create({ username });
-    res.status(200).json(user);
+    res.status(201).json(user);
   } catch (err) {
     res.status(400).json(err);
   }
 }
 
-module.exports = { createUser };
+async function getUsers(req, res) {
+  try {
+    const allUsers = await userModel.find();
+    res.status(200).json(allUsers);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+module.exports = { createUser, getUsers };
